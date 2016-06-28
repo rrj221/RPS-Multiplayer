9cb15696b9aa30c47e57090be07aab3fcb85500e
JARRELL <RJARRELL@nylim.nt.newyorklife.com>
2016-06-27 22:17:25 -0400

    some logic to display stuff to screen working

--- assets/javascript/rps-script.js
+++ assets/javascript/rps-script.js
@@ -3,6 +3,13 @@
 // $('h1').on('click', function () {
 // 	alert('jquery!');
 // });
+
+var choicesArray = ['Rock', 'Paper', 'Scissors'];
+//SET THIS TO ONE FOR DEBUGGING - SET TO 0 LATER!!!!!!!!!!!///////////////////////////////////////
+//*************************
+var currentPlayer = 0;
+//*************************
+var canUpdateBox = true;
   
 var config = {
     apiKey: "AIzaSyAUfn3q5rPIf_V9-dSRu30GntbrF_LzPWE",
@@ -81,7 +88,7 @@ function login(name, playerNumber) {
 	// showWinsLosses(playerNumber);
 
 	if (playerNumber === 1) {
-		alert('okay');
+		// alert('okay');
 		//initialize players object in firebase
 		dbRef.set({
 			players: {
@@ -92,30 +99,35 @@ function login(name, playerNumber) {
 				}
 			}		
 		});
+		currentPlayer = 1;
 	} 
 	else if (playerNumber === 2) {
-		alert('yay');
+		// alert('yay');
 		dbRef.child('players').update({
 			2: {
 				losses: 0,
 				name: name,
 				wins: 0,
-			}
+			},
+			turn: 1
 		});
+		currentPlayer = 2;
 	}	
 };
 
-function loginPlayerTwo(name, playerNumber) {
-	$('.usernameForm').hide();
-	var playerMessageDiv = $('<div>', {
-		text: "Hi "+name+"! You are Player "+playerNumber
-	});
-	playerMessageDiv.appendTo($('.topPart'));
+// function loginPlayerTwo(name, playerNumber) {
+// 	$('.usernameForm').hide();
+// 	var playerMessageDiv = $('<div>', {
+// 		text: "Hi "+name+"! You are Player "+playerNumber
+// 	});
+// 	playerMessageDiv.appendTo($('.topPart'));
 
-	playerUpdateFirebase(playerNumber, name);
-	hideWaitingText(playerNumber);
-	showWinsLosses(playerNumber);
-}
+// 	playerUpdateFirebase(playerNumber, name);
+// 	hideWaitingText(playerNumber);
+// 	showWinsLosses(playerNumber);
+// }
+
+
 
 
 $('.usernameForm').on('submit', function () {
@@ -157,5 +169,93 @@ dbRef.on('value', function (Snapshot) {
 	displayWinsLosses(2, p1Wins, p1Losses);
 	var player2Name = Snapshot.val().players[2].name;
 	displayName(2, player2Name);
+
+	//turn one
+	if (Snapshot.val().players.turn === 1) {
+		if (currentPlayer === 1) {
+			showChoices(1, choicesArray);
+		}
+	}
+
+	//turn two
+	if (Snapshot.val().players.turn === 2) {
+		if (currentPlayer === 2 && canUpdateBox) {
+			showChoices(2, choicesArray);
+			canUpdateBox = false;
+		}
+	}
 });
 
+function showChoices(playerNumber, choicesArray) { 
+	var choicesDiv = $('<div>', {class: 'choicesDiv'});
+
+	//create DOM elements and append to choicesDiv
+	for (var i = 0; i < choicesArray.length; i++) {
+		var choice = choicesArray[i];
+		var choiceDiv = $('<div>', {
+			class: 'choice',
+			'data-choice': choice,
+			text: choice
+		});
+		choicesDiv.append(choiceDiv);
+	}
+	//append choicesDiv to DOM
+	var DivToAppendToID = "#player"+playerNumber+"Box";
+	choicesDiv.appendTo(DivToAppendToID);
+};
+
+$('.box').on('click', '.choice', function () {
+	console.log($(this).data('choice'));
+	var choiceClicked = $(this).data('choice'); //ie 'rock', 'paper'
+	var currentTurn = getTurn();
+
+	//turn one
+	// if (currentTurn === 1) {
+	// 	if (currentPlayer === 1) {
+	// 		showChoices(1, choicesArray);
+	// 	}
+	// }
+
+	//turn two
+	// if (currentPlayer === 2) {
+	// 	showChoices(2, choicesArray);
+	// }
+
+
+
+
+
+	incrementTurn();
+	addChoiceToFirebase(1, choiceClicked);
+
+
+	// if (currentTurn === 1) {
+
+	// }
+
+
+});
+
+function incrementTurn() {
+	dbRef.once('value', function (snapshot) {
+		var currentTurn = snapshot.val().players.turn;
+		dbRef.child('players').update({
+			turn: currentTurn + 1
+		});
+	});
+};
+
+function getTurn() {
+	var currentTurn;
+	dbRef.once('value', function (snapshot) {
+		currentTurn = snapshot.val().players.turn;
+		// return 1;
+	});
+	return currentTurn;
+};
+
+function addChoiceToFirebase(playerNumber, choice) {
+	dbRef.child('players').child(1).update({
+		choice: choice
+	});
+};
